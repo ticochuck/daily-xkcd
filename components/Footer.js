1a0cbdc1c36206c53c747cad388f6c584958c700
ticochuck <chucklivilla@gmail.com>
2020-10-06 21:09:58 -0700

    lab39

---
+++ components/Footer.js
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+export default function Footer(props) {
+    
+    const currentNum = props.comicNum;
+    const nums = [];
+
+    for (let i = currentNum; i >= currentNum - 10; i--) {
+        nums.push(i)
+    }
+    
+    return (
+        <footer>
+            <h2>Previous {nums.length}</h2>
+            {nums.map(num => (
+                <span key={num}>
+                    <Link href="/num/[id].js" as={`/num/${num}`}>
+                        <a>#{num}</a>
+                    </Link>
+                </span>
+            ))}
+            <style jsx>{`
+                span {
+                    margin-right: 5px;
+                }
+            `}
+            </style>
+        </footer>
+    )
+}
